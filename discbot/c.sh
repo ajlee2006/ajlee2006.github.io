558e981c0a1a774ca4c28779a5b524f77bb5f6d8
AJ Lee <ajlee2006@gmail.com>
2021-02-08 13:02:50 +0800

    Create c.sh

---
+++ discbot/c.sh
@@ -0,0 +1,1 @@
+curl -v -H "Content-Type: application/json" -X POST -d '{"content":"C"}' https://discord.com/api/webhooks/808124010042228747/sCf0sHAygxJ2ktUywjOQuHqYxSDjgEPMRJKVMyaQekTOulQGmwrbL5XfWL199L0PG9e9
