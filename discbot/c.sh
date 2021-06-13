f10dc6f4bb92f55153db1abf8504d987c24a5da9
Andrew Lee <ajlee2006@gmail.com>
2021-06-13 18:11:47 +0800

    Delete c.sh

--- discbot/c.sh
+++
@@ -1,1 +0,0 @@
-curl -v -H "Content-Type: application/json" -X POST -d '{"content":"C"}' https://discord.com/api/webhooks/808124010042228747/sCf0sHAygxJ2ktUywjOQuHqYxSDjgEPMRJKVMyaQekTOulQGmwrbL5XfWL199L0PG9e9
